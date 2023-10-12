69397b6767dc5cbe3fc55bd038a1fa34de256771
anikmondol <anikmondol558363@gmil.com>
2023-10-12 21:29:51 +0600

    add ay food

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,22 +1,41 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
+
 
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
+    const [visitedCountries, setVisitedCountries] = useState([])
     useEffect(() =>{
         fetch(`https://restcountries.com/v3.1/all`)
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
+    const handleVisitedCountry = country => {
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries)
+    }
     return (
         <div>
             <h2>Countries: {countries.length}</h2>
+            <div>
+               <h4>Visited Countries: {visitedCountries.length}</h4> 
+               <ul>
+                {
+                    visitedCountries.map(country => <p className="Text" key={country.cca3}>{country.name.common}</p>)
+                }
+               </ul>
+            </div>
+            <div className="Country-Container">
             {
-                countries.map(country => <Country key={country.cca3}
+                countries.map(country => <Country 
+                handleVisitedCountry={handleVisitedCountry}
+                key={country.cca3}
                 country={country}></Country>)
             }
+            </div>
            
         </div>
     );
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import './Country.css'
 
-const Country = ({ country }) => {
-    console.log(country)
-    const { name, flags, population, area, capital, region, independent} = country;
+const Country = ({ country, handleVisitedCountry }) => {
+    const { name, flags, population, area, capital, region, independent, cca3 } = country;
+
+    const [visited, setVisited] = useState(false)
+    const handleVisited = () => {
+        setVisited(!visited)
+    }
+
     return (
-        <div className='Country'>
-            <h3>Name: {name.common}</h3>
-            <img src={flags.png} alt="" />
+        <div className={`Country ${visited && 'Visited'}`}>
+            <h3 style={{color: visited ? 'blue' : 'red'}}>Name: {name.common}</h3>
+            <img className='Pic' src={flags.png} alt="" />
             <p>population: {population}</p>
             <p>Capital: {capital}</p>
             <p>Area: {area}</p>
             <p>Region: {region}</p>
-            <p>Independent: {independent?"Yes Independent": "No"}</p>
+            <p>Independent: {independent ? "Yes Independent" : "No"}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button className='Visited-Button' onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            <h5 className='Visited-Text'>{visited ? 'I have visited this country' : 'I want to visited'}</h5>
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
         </div>
     );
 };
